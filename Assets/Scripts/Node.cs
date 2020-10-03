1b132f800829d501923a9af39f051c7edb79596f
gquibao <gquibao@gmail.com>
2020-10-03 15:22:05 -0300

    Adding project to a git repository

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Node
+{
+    public string nodeName;
+    public List<Node> _childNodes;
+
+    public Node(List<string> nodeNames)
+    {
+        if (nodeNames.Count == 0) return;
+        nodeName = nodeNames[0];
+        nodeNames.RemoveAt(0);
+        if (nodeNames.Count == 0) return;
+        CreateChildNodes(nodeNames);
+    }
+
+    public void CreateChildNodes(List<string> nodeNames)
+    {
+        if(_childNodes == null) _childNodes = new List<Node>();
+        var currentName = nodeNames[0];
+        var nextNode = _childNodes?.Find(node => node.nodeName == currentName);
+        if (nextNode != null)
+        {
+            nodeNames.RemoveAt(0);
+            if (nodeNames.Count == 0) return;
+            nextNode.CreateChildNodes(nodeNames);
+        }
+        else
+        {
+            Node newNode;
+            if (currentName.Contains("|"))
+            {
+                nodeNames.RemoveAt(0);
+                var names = currentName.Split('|').ToList();
+                names.ForEach(name =>
+                {
+                    var newNameList = nodeNames;
+                    newNameList.Insert(0, name);
+                    newNode = new Node(newNameList);
+                    _childNodes?.Add(newNode);
+                });
+            }
+            else
+            {
+                newNode = new Node(nodeNames);
+                _childNodes?.Add(newNode);
+            }
+        }
+    }
+
+    public void PrintLeaf()
+    {
+        if (_childNodes == null)
+        {
+            Debug.Log($"{nodeName}/");
+            return;
+        }
+        _childNodes.ForEach(node =>
+        {
+            node.PrintLeaf(nodeName);
+        });
+    }
+
+    private void PrintLeaf(string path)
+    {
+        var currentPath = $"{path}/{nodeName}";
+        if (_childNodes ==  null)
+        {
+            Debug.Log(currentPath);
+        }
+        else
+        {
+            _childNodes.ForEach(node =>
+            {
+                node.PrintLeaf(currentPath);
+            });
+        }
+    }
+
+    public void PrintTree(int level = 0)
+    {
+        var text = "";
+        for (var i = 0; i < level; i++)
+        {
+            text += "-";
+        }
+
+        text += nodeName;
+        Debug.Log(text);
+        if (_childNodes == null) return;
+        level++;
+        _childNodes.ForEach(node => node.PrintTree(level));
+    }
+}
---
+++ Assets/Scripts/TreeManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+public class TreeManager : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField inputField;
+    private List<Node> _trees;
+
+    private void Start()
+    {
+        _trees = new List<Node>();
+        inputField.onSubmit.AddListener(InsertNodes);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            _trees.ForEach(node => node.PrintLeaf());
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            _trees.ForEach(node => node.PrintTree());
+        }
+    }
+
+    private void InsertNodes(string inputText)
+    {
+        var nodeNames = FormatString(inputText);
+            
+        var nextNode = _trees.Find(node => node.nodeName == nodeNames[0]);
+        if (nextNode != null)
+        {
+            nodeNames.RemoveAt(0);
+            nextNode.CreateChildNodes(nodeNames);
+        }
+        else
+        {
+            var newNode = new Node(nodeNames);
+            _trees.Add(newNode);
+        }
+    }
+
+    private List<string> FormatString(string textToFormat)
+    {
+        if (textToFormat[0] == '/')
+        {
+            textToFormat = textToFormat.Remove(0, 1);
+        }
+        var split = textToFormat.Split('/').ToList();
+        return split;
+    }
+}
